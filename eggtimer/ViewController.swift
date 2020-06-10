61dd7dd5c8ca75ec1824d4ec628920d5400bab33
VyomUnadkat <18560093+VyomUnadkat@users.noreply.github.com>
2020-06-11 00:18:26 +0530

    initial commit

--- eggtimer/ViewController.swift
+++ eggtimer/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var timer = Timer()
+    
+    var i : Int = 240
+    
+    @IBOutlet weak var output: UILabel!
+    @objc func timercount()
+    {
+        if(i >= 0)
+        {
+            output.text = String (i)
+            i = i - 1
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func play(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timercount), userInfo: nil, repeats: true)
+    }
+    
+
+    @IBAction func pause(_ sender: Any) {
+        
+        timer.invalidate()
     }
 
-
+    @IBAction func plusTen(_ sender: Any) {
+        
+        i = i + 10
+        
+        output.text = String(i)
+    }
+    @IBAction func minusTen(_ sender: Any) {
+        if ( i > 10)
+        {
+        i = i - 10
+        
+        output.text = String(i)
+        }
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        i = 240
+        output.text = String(i)
+    }
 }
 
